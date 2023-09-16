6682ef62706618015ac37c7be8c3ff2a902570b5
dos-m0nk3y <53410240+dos-m0nk3y@users.noreply.github.com>
2023-09-16 06:52:26 -0400

    Update program counter module to use custom clock and reset

--- MIPS I soft processor/single-cycle processor/pc.v
+++ MIPS I soft processor/single-cycle processor/pc.v
@@ -1,12 +1,12 @@
 module pc (
-    input wire CLK100MHZ,
-    input wire ck_rst,
+    input wire clk,
+    input wire rst,
     input wire [31:0] pc_i,
     output reg [31:0] pc_o
 );
 
-  always @(posedge CLK100MHZ or negedge ck_rst) begin
-    if (!ck_rst) pc_o <= 32'h00000000;
+  always @(posedge clk) begin
+    if (!rst) pc_o <= 32'h00000000;
     else pc_o <= pc_i;
   end
 
